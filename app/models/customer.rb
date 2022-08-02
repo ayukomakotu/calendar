5379e2a6edd4cf31dc269497309ba120aa75dfb4
ayukomakotu <yamamesan708@gmail.com>
2022-08-02 19:15:31 +0900

    コメントアウトされていた関連付けを元に戻した

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
-  # has_many :events
-  #Customer.ordersで顧客の受注履歴を取得するため
-  # has_many :orders,            through: :events
+  has_many :events
+  # Customer.ordersで顧客の受注履歴を取得するため
+  has_many :orders,            through: :events
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
-  # belongs_to :event
-  # belongs_to :route
-  # belongs_to :item
-  # belongs_to :user
+  belongs_to :event
+  belongs_to :route
+  belongs_to :item
+  belongs_to :user
   validates :route_id, presence: true
   validates :item_id, presence: true
 
