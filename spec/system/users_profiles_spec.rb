2b46512ba95f2a5476424f6012b604bed70f7a4d
ayukomakotu <yamamesan708@gmail.com>
2022-08-05 21:34:45 +0900

    fix users_profile_spec

--- spec/system/users_profiles_spec.rb
+++ spec/system/users_profiles_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "UsersProfiles", type: :system do
-  before do
-    @user = FactoryBot.create(:michael)
-  end
+  
+  let!(:user1)  { FactoryBot.create(:user1) }
 
   it "タイトルは正しいか" do
-    visit user_path(@user)
-    expect(page).to have_title @user.name
+    visit user_path(user1)
+    expect(page).to have_title user1.name
   end
   it "正しいユーザーの情報が表示されているか" do
-    visit user_path(@user)
-    expect(page).to have_content @user.name
+    visit user_path(user1)
+    expect(page).to have_content user1.name
   end
 end
