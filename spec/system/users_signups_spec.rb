7509bfd10cd17628ebcc3085fed00e072ef78f3c
ayukomakotu <yamamesan708@gmail.com>
2022-08-05 22:52:34 +0900

    event_table_spec追加（未完成）

--- spec/system/users_signups_spec.rb
+++ spec/system/users_signups_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSignups", type: :system do
-  pending("のちに実装")
-    context "パラメータが無効な場合" do
-    end
-  end
+  # pending("のちに実装")
+  #   context "パラメータが無効な場合" do
+  #   end
+  # end
 
-  context "パラメータが有効な場合" do
-    it "ユーザーが増えること" do
-      visit new_user_registration_path
-      expect do
-        fill_in 'user[name]',                  with: 'test_name'
-        fill_in 'user[email]',                 with: 'test@example.com'
-        fill_in 'user[password]',              with: 'password'
-        fill_in 'user[password_confirmation]', with: 'password'
-        click_on '新規登録'
-      end.to change(User, :count).by(1)
-    end
+  # context "パラメータが有効な場合" do
+  #   it "ユーザーが増えること" do
+  #     visit new_user_registration_path
+  #     expect do
+  #       fill_in 'user[name]',                  with: 'test_name'
+  #       fill_in 'user[email]',                 with: 'test@example.com'
+  #       fill_in 'user[password]',              with: 'password'
+  #       fill_in 'user[password_confirmation]', with: 'password'
+  #       click_on '新規登録'
+  #     end.to change(User, :count).by(1)
+  #   end
 end
