179200b7456d93a80f816b2cb2dd8df7e7c09133
ayukomakotu <yamamesan708@gmail.com>
2022-08-06 14:34:55 +0900

    fix spec/system/events_table_spec.rb

--- spec/system/events_table_spec.rb
+++ spec/system/events_table_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Event", type: :system do
     let!(:item)   { FactoryBot.create(:item) }
     let!(:route)  { FactoryBot.create(:route) }
     
-    it "非ログイン状態でログイン画面に戻される" do
+    it "非ログイン状態でログイン画面に戻される, ログイン後リダイレクトするか" do
         visit events_path
         visit current_path
         expect(current_path).to eq login_path
@@ -29,26 +29,25 @@ RSpec.describe "Event", type: :system do
         fill_in "event_form[telephone]", with: "sample_telephone"
         fill_in "event_form[start_time]", with: Date.today
         select "１号車", from: "event_form_car" #セレクトボックスのidを指定
-        fill_in "event_form[start]", with: "2022-08-05 09:00:00 +0900"
-        fill_in "event_form[finish]", with: "2022-08-05 09:00:00 +0900"
         select item.name, from: "event_form_item_id" #セレクトボックスのidを指定
         fill_in "event_form_number", with: 100
         fill_in "event_form_price",  with: 1000000
         select route.name, from: "event_form_route_id" #セレクトボックスのidを指定
+        fill_in "event_form_start", with: "09:00"
+        fill_in "event_form_finish", with: "11:00"
         click_button "登録"
-        save_and_open_page
         visit current_path
 
         #施工予定が追加され、正しく表示されているか
-        expect(page).to include "sample_last_name"
+        expect(page).to have_content "sample_last_name"
         # expect(page).to include "sample_first_name"
-        expect(page).to include "sample_address"
+        expect(page).to have_content "sample_address"
         # expect(page).to include "sample_telephone"
-        expect(page).to include user1.name
-        expect(page).to include item.name
-        expect(page).to include "100"
-        expect(page).to include item.unit
-        expect(page).to include "1000000"
-        expect(page).to include "09:00 - 11:00"
+        expect(page).to have_content user1.name
+        expect(page).to have_content item.name
+        expect(page).to have_content "100"
+        expect(page).to have_content item.unit
+        expect(page).to have_content "1000000"
+        expect(page).to have_content "09:00 - 11:00"
     end
 end
