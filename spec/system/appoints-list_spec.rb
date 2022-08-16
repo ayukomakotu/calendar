0210e014a47f6102d79cd994fdd265b46f222a47
ayukomakotu <yamamesan708@gmail.com>
2022-08-16 23:26:18 +0900

    appoint_list_spec修正中

--- spec/system/appoints-list_spec.rb
+++ spec/system/appoints-list_spec.rb
@@ -25,5 +25,22 @@ RSpec.describe "AppointsList", type: :system do
         first(".new").click
         expect(current_path).to eq new_appoint_path
         # 新しくappointを作成する
+        fill_in "appoint[name]", with: "田中"
+        fill_in "appoint[address]", with: "松江" 
+        fill_in "appoint[telephone]", with: "xxx-xxxx"
+        fill_in "appoint[day]", with: Date.today
+        fill_in "appoint[time]", with: "09:00"
+        fill_in "appoint[kind]", with: "推進"
+        click_button "追加"
+        # appoints/indexにリダイレクトしているか
+        expect(current_path).to eq appoints_path
+        # フラッシュメッセージが表示されているか
+        # expect(page).to "予定を追加しました"
+        # 新しいappointが表示されているか
+        expect(page).to have_content "田中"
+        expect(page).to have_content "松江"
+        expect(page).to have_content "xxx-xxxx"
+        expect(page).to have_content "09:00"
+        expect(page).to have_content "推進"
     end 
 end
