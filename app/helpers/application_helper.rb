c2ac3877039879728090e0e215bd68553c26e4e2
ayukomakotu <yamamesan708@gmail.com>
2022-08-17 14:49:12 +0900

    タイトル修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "dedama_jiman"
+    base_title = "コダマ受注管理"
     if page_title.empty?
       base_title
     else
