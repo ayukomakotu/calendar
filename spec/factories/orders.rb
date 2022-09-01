49e169eb6dba33aabb665a0dafecdf75a91d3e4b
ayukomakotu <yamamesan708@gmail.com>
2022-09-01 21:31:03 +0900

    bundle update & rspec修正

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,13 +2,13 @@ FactoryBot.define do
   factory :order do
     number { 100 }
     price  { 100000 }
-    day    { "#{Date.today}" }
+    day    { "#{Date.today.end_of_month}" }
   end
 
   factory :order2, class: Order do
     number { 100 }
     price  { 200000 }
-    day    { "#{Date.yesterday}" }
+    day    { "#{Date.today.end_of_month.yesterday}" }
     created_at { Time.now.prev_day }
   end
 
